b5f7a736ebc66a79fd03673147ac91f2ede5520e
brume <brume@localhost.localdomain>
2020-08-13 20:30:09 +0100

    added enum  unit tests

--- src/test/java/com/flexion/converter/domain/TemperatureEnumTest.java
+++ src/test/java/com/flexion/converter/domain/TemperatureEnumTest.java
@@ -6,12 +6,33 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
 
 
 @SpringBootTest
 @Slf4j
 public class TemperatureEnumTest {
+    @Autowired
+    private TemperatureService temperatureService;
+    @Test
+    public void correctTemperatureConversion() {
+        var result = Temperatures.FAHRENHEIT.convert(84.2, Temperatures.RANKINE);
+        Assertions.assertNotEquals(result, 543.8);
+    }
+    @Test
+    public void getTempFromString() {
+        var getTempFromString = Temperatures.getTemperatures("Fahrenheit");
+        Assertions.assertEquals(getTempFromString, Temperatures.FAHRENHEIT);
+    }
+    @Test
+    public void roundedToTenth() {
+        var getTempFromString = Temperatures.getTemperatures("Fahrenheit");
+        var authoritativeAnswer = getTempFromString.convert(84.2, Temperatures.RANKINE);
+        var roundedToTenthResult = temperatureService.roundToTenths(authoritativeAnswer);
+
+        Assertions.assertEquals(roundedToTenthResult, 543.9);
+
+    }
+
 
 
 }
--- src/test/java/com/flexion/converter/domain/VolumeEnumTest.java
+++ src/test/java/com/flexion/converter/domain/VolumeEnumTest.java
@@ -1,5 +1,6 @@
 package com.flexion.converter.domain;
 
+import com.flexion.converter.usecase.TemperatureService;
 import com.flexion.converter.usecase.VolumeService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Assertions;
@@ -11,5 +12,24 @@ import org.springframework.boot.test.context.SpringBootTest;
     @SpringBootTest
     @Slf4j
     public class VolumeEnumTest  {
+        @Autowired
+        private TemperatureService temperatureService;
+        @Test
+        public void correctVolumeConversion() {
+            var result = Volumes.CUPS.convert(25.6, Volumes.LITERS);
+            Assertions.assertEquals(result, 6.0566528);
+        }
+        @Test
+        public void getVolumeFromString() {
+            var getVolumeFromString = Volumes.getVolumes("cubic-inches");
+            Assertions.assertNotEquals(getVolumeFromString, Volumes.CUPS);
+        }
+        @Test
+        public void roundedToTenth() {
+            var authoritativeAnswer = Volumes.CUPS.convert(25.6, Volumes.LITERS);
+            var roundedToTenthResult = temperatureService.roundToTenths(authoritativeAnswer);
 
+            Assertions.assertEquals(roundedToTenthResult, 6.1);
+
+        }
 }
